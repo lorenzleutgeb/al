e8c5dc23dd296fa5120d70cb4057b15bd0863139
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2018-05-10 11:33:42 +0200

    Improve rendering scripts

--- queens.py
+++ queens.py
@@ -2,5 +2,12 @@
 
 from sys import stdin
 
-print('\n'.join([str(8 - j) + ''.join(['X' if i == int(ln) else '.' for i in range(8)]) for j, ln in enumerate(stdin)]))
-print(' abcdefgh')
+print(
+    '\n' +
+    '  A Solution for The Eight Queens Problem\n'
+    '     ╔═══╤═══╤═══╤═══╤═══╤═══╤═══╤═══╗\n'+
+    '     ╟───┼───┼───┼───┼───┼───┼───┼───╢\n'.join(['   ' + str(8 - j) + ' ║ ' + ' │ '.join(['♕' if i == int(ln) else ' ' for i in range(8)]) + ' ║\n' for j, ln in enumerate(stdin)]) +
+    '     ╚═══╧═══╧═══╧═══╧═══╧═══╧═══╧═══╝\n' +
+    '       a   b   c   d   e   f   g   h' +
+    '\n'
+)
--- river.py
+++ river.py
@@ -3,69 +3,63 @@
 from sys import stdin
 
 state = {
-    'm': 3,
-    'c': 3,
+    'm' : 3,
+    'c' : 3,
     'mb': 0,
     'cb': 0,
-    'b': False
+    'b' : False
 }
+
+N = 3                  # number of missionaries/cannibals
+R = range(1, 1 + N)
+
+M = '👼'                # missionaries
+C = '👹'                # cannibals
+W = '\033[96m≈\033[0m' # water/wave (and ANSI control codes for blue color)
+S = ' '                # space
+T = '⛰️⛰️'
 
 cnt = 0
 
+def line(neutral, spec):
+    result = ''
+    for it in spec:
+        if type(it) is int:
+            result = result + neutral * it
+        else:
+            c, t = it
+            result = result + c * t
+    return result
+
 def emit():
     global cnt
+
+    print()
+
+
     cnt = cnt + 1
-    print(str(cnt))
+    print("Day " + str(cnt) + ":")
+
     #print(state)
+
     m, c, mb, cb, b = state['m'], state['c'], state['mb'], state['cb'], state['b']
-    print(
-        '  ' +
-        ''.join(['M' if i > m else ' ' for i in range(1, 4)]) +
-        ' ' +
-        ''.join(['C' if i > c else ' ' for i in range(1, 4)])
-    )
-    print('~~~~~~~~~~~')
-    print('~~~~~~~~~~~')
-    print(
-        '  ' +
-        ''.join(['M' if i <= m else ' ' for i in range(1, 4)]) +
-        ' ' +
-        ''.join(['C' if i <= c else ' ' for i in range(1, 4)])
-    )
+    print(line(S, [6, m * 2, (M, N - m), 4, (C, N - c)]))
+    print(W * ((2 * 3 + 2 + 2 * N) * 2))
+    print(line(S, [6, (M, m), (N - m) * 2, 4, (N - c) * 2, (C, c)]))
 
     if m + c == 0:
         return
 
-    if b:
-        c = c - cb
-        m = m - mb
+    print("Action:")
 
-    print()
-    print(
-        '  ' +
-        ''.join(['M' if i > m else ' ' for i in range(1, 4 - mb)]) +
-        ' ' +
-        ''.join(['C' if i > c else ' ' for i in range(1, 4 - cb)])
-    )
-    #print('~~~~~~~')
-    if state['b']:
-        print('~~~~~~~~~~~')
-    print(
-        '~~' +
-        ''.join(['M' if i <= mb else '~' for i in range(1, 4)]) +
-        '~' +
-        ''.join(['C' if i <= cb else '~' for i in range(1, 4)]) +
-        '~~'
-    )
-    #print('~~~~~~~')
-    if not state['b']:
-        print('~~~~~~~~~~~')
-    print(
-        '  ' +
-        ''.join(['M' if i <= m else ' ' for i in range(1, 4 - mb)]) +
-        ' ' +
-        ''.join(['C' if i <= c else ' ' for i in range(1, 4 - cb)])
-    )
+    srm = mb if not b else 0
+    src = cb if not b else 0
+    swm = mb * int(b)
+    swc = cb * int(b)
+
+    print(line(S, [6, m * 2, srm * 2, (M, N - m - srm), 4, (C, N - c - src)]))
+    print(line(W, [11, (S, 1), (M, mb), (C, cb), (S, 2 - (mb + cb)), (S, 1), 11]) + ' ' + ('↑' if b else '↓'))
+    print(line(S, [6, (M, m - swm), (N - m - srm) * 2, 4, (N - c + swc) * 2, (C, c - swc)]))
     print()
 
 # Discard first line.
